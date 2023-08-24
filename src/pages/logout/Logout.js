be3a3815c2421471fa86c7c9c9bb2df0ca1cdf0c
Doflamingo1998 <le.d.quan1998@gmail.com>
2023-08-24 21:10:48 +0700

    Update Logout.js

--- src/pages/logout/Logout.js
+++ src/pages/logout/Logout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { logoutUser } from "../../redux/actions/authActions"
+import './Logout.css';
 
 const Logout = ({ logoutUser }) => {
     const handleLogout = () => {
