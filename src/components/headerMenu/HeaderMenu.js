71398aaee4d00459396ed7654a3cc8e61d2cce64
VanLeDinh96 <dinhvanle.it@gmail.com>
2023-08-29 13:32:27 +0700

    update logout

--- src/components/headerMenu/HeaderMenu.js
+++ src/components/headerMenu/HeaderMenu.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
+import Modal from "react-modal";
+import { connect } from "react-redux";
 import Logout from "../../pages/logout";
+import { logoutUser } from "../../redux/actions/authActions";
 import './HeaderMenu.css';
 
-const HeaderMenu = () => {
+const HeaderMenu = ({ logoutUser }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [isLogoutOpen, setIsLogoutOpen] = useState(false);
 
@@ -11,35 +14,64 @@ const HeaderMenu = () => {
     };
 
     const closeMenu = () => {
-        console.log("closeMenu before update isMenuOpen: ", isMenuOpen);
-        console.log("closeMenu before update isLogoutOpen: ", isLogoutOpen); setIsMenuOpen(false);
+        setIsMenuOpen(false);
         setIsLogoutOpen(true);
-        console.log("closeMenu after update isMenuOpen: ", isMenuOpen);
-        console.log("closeMenu after update isLogoutOpen: ", isLogoutOpen);
     };
 
-    const closeLogout = () => {
+    const handleLogout = () => {
+        logoutUser();
+        closeModal();
+    };
+
+    const closeModal = () => {
         setIsLogoutOpen(false);
     };
 
     return (
-        <div className={`dropdown ${isMenuOpen ? 'active' : ''}`}>
-            <div className="menu-icon" onClick={toggleMenu}>
-                {isMenuOpen ? (
-                    <img src="/icon/icon-menu-close.svg" alt="Closed Menu" />
-                ) : (
-                    <img src="/icon/icon-menu.svg" className="icon-2" alt="Open Menu" />
+        <Fragment>
+            <div className={`dropdown ${isMenuOpen ? 'active' : ''}`}>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    {isMenuOpen ? (
+                        <img src="/icon/icon-menu-close.svg" alt="Closed Menu" />
+                    ) : (
+                        <img src="/icon/icon-menu.svg" className="icon-2" alt="Open Menu" />
+                    )}
+                </div>
+                {isMenuOpen && (
+                    <div className="dropdown-content">
+                        <div><Logout onCloseMenu={closeMenu} /></div>
+                        <div>Mục 2</div>
+                        <div>Mục 3</div>
+                    </div>
                 )}
             </div>
-            {isMenuOpen && (
-                <div className="dropdown-content">
-                    <div><Logout isLogoutOpen={isLogoutOpen} onCloseMenu={closeMenu} onCloseLogout={closeLogout} /></div>
-                    <div>Mục 2</div>
-                    <div>Mục 3</div>
+
+            <Modal
+                isOpen={isLogoutOpen}
+                onRequestClose={closeModal}
+                ariaHideApp={false}
+                className="modal"
+                overlayClassName="overlay"
+            >
+                <div className="modal__close" onClick={closeModal}>
+                    &times;
                 </div>
-            )}
-        </div>
+                <h2 className="modal__title">Do you really want to log out?</h2>
+                <div className="modal__buttons">
+                    <button className="modal__button cancel" onClick={closeModal}>
+                        No
+                    </button>
+                    <button className="modal__button submit" onClick={handleLogout}>
+                        Yes
+                    </button>
+                </div>
+            </Modal>
+        </Fragment>
     );
 };
 
-export default HeaderMenu;
+const mapDispatchToProps = {
+    logoutUser
+};
+
+export default connect(null, mapDispatchToProps)(HeaderMenu);
--- src/pages/logout/Logout.js
+++ src/pages/logout/Logout.js
@@ -1,47 +1,16 @@
 import React, { Fragment } from "react";
-import Modal from "react-modal";
 import { connect } from "react-redux";
-import { logoutUser } from "../../redux/actions/authActions";
 
-const Logout = ({ logoutUser, onCloseMenu, isLogoutOpen, onCloseLogout }) => {
-    const handleLogout = () => {
-        logoutUser();
-        closeModal();
-    };
-
+const Logout = ({ onCloseMenu }) => {
     const openModal = () => {
         onCloseMenu();
-    };
-
-    const closeModal = () => {
-        onCloseLogout();
     };
 
     return (
         <Fragment>
             <div onClick={openModal}>Log out</div>
-            <Modal
-                isOpen={isLogoutOpen}
-                onRequestClose={closeModal}
-                ariaHideApp={false}
-                className="modal"
-                overlayClassName="overlay"
-            >
-                <div className="modal__close" onClick={closeModal}>
-                    &times;
-                </div>
-                <h2 className="modal__title">Do you really want to log out?</h2>
-                <div className="modal__buttons">
-                    <button className="modal__button cancel" onClick={closeModal}>
-                        No
-                    </button>
-                    <button className="modal__button submit" onClick={handleLogout}>
-                        Yes
-                    </button>
-                </div>
-            </Modal>
         </Fragment>
     );
 };
 
-export default connect(null, { logoutUser })(Logout);
+export default connect(null, null)(Logout);
